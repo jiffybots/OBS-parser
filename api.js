08efb12eeb45ef9edef714b4a7051ad56e738728
Preethi Kasireddy <iam.preethi.k@gmail.com>
2016-12-09 11:09:57 -0800

    Set up express server

---
+++ api.js
@@ -0,0 +1,8 @@
+
+module.exports = (app) => {
+  app.post('/receive-xml', function(req, res, next) {
+
+    // req.body contains the parsed xml
+
+  })
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const xmlparser = require('express-xml-bodyparser');
+const router = express.Router();
+const port = process.env.PORT || 3000;
+
+// Define middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(xmlparser());
+
+// Define main API route
+app.use('/api', router);
+require('./api')(router);
+
+// Listen on port 3000
+app.listen(port, (error) => {
+  if (error) {
+    console.error(error.stack);
+  } else {
+    console.info('==> 🌎  Listening on port %s.', port);
+  }
+});
